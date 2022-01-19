13f83766e97753ccafa4ac888c8f3425001f105f
Arman Khan <58773113+TrickySoft@users.noreply.github.com>
2022-01-19 23:38:56 +0530

    structure

--- src/app/pages/home/category/index.js
+++ src/app/pages/home/category/index.js
@@ -6,55 +6,55 @@ const Category = () => {
     return (
         <div>
             <div>
-<h1 className='featured-container__featured-title'>Featured</h1>
-<div className='allpost__right-category'>
+{/* <h1 className=''>Featured</h1> */}
+<div className='category__right-category'>
 
-<div className='allpost__right-category__content'>
+<div className='category__right-category__content'>
   <a href='' className='category-title'>
     Want fluffy Japanese pancakes ?
   </a>
-    <ul className='allpost__card-ul'>
+    <ul className='category__card-ul'>
                 <li><i className='fa fa-clock-o' aria-hidden='true'> Today</i></li>
                 <li><i className='fa fa-eye' aria-hidden='true'></i> 150k views</li>
               </ul>
 </div>
-<div className='allpost__right-category__img'>
+<div className='category__right-category__img'>
 <img src={cardImg} className='category-image' alt='' />
 </div>
 </div>
-<div className='allpost__right-category'>
+<div className='category__right-category'>
 
-<div className='allpost__right-category__content'>
+<div className='category__right-category__content'>
   <a href='' className='category-title'>
     Want fluffy Japanese pancakes ?
   </a>
-    <ul className='allpost__card-ul'>
+    <ul className='category__card-ul'>
                 <li><i className='fa fa-clock-o' aria-hidden='true'> Today</i></li>
                 <li><i className='fa fa-eye' aria-hidden='true'></i> 150k views</li>
               </ul>
 </div>
-<div className='allpost__right-category__img'>
+<div className='category__right-category__img'>
 <img src={cardImg} className='category-image' alt='' />
 </div>
 </div>
-<div className='allpost__right-category'>
+<div className='category__right-category'>
 
-<div className='allpost__right-category__content'>
+<div className='category__right-category__content'>
   <a href='' className='category-title'>
     Want fluffy Japanese pancakes ?
   </a>
-    <ul className='allpost__card-ul'>
+    <ul className='category__card-ul'>
                 <li><i className='fa fa-clock-o' aria-hidden='true'> Today</i></li>
                 <li><i className='fa fa-eye' aria-hidden='true'></i> 150k views</li>
               </ul>
 </div>
-<div className='allpost__right-category__img'>
+<div className='category__right-category__img'>
 <img src={cardImg} className='category-image' alt='' />
 </div>
 </div>
 </div>
-        </div>
+</div>
     )
 }
 
-export default Category;
+export default React.memo(Category);
--- src/app/pages/home/components/allpost/index.js
+++ src/app/pages/home/components/allpost/index.js
@@ -2,94 +2,67 @@
 
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { getPostList } from '../../../../utils/apiCalls'
+import { getPostList } from '../../../../utils/apiCalls';
 import moment from 'moment';
 import './index.scss';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import ReadMore from '../readmore';
-import cardImg from '../../../../../assets/images/card-1.jpg'
+import cardImg from '../../../../../assets/images/card-1.jpg';
 import Category from '../../category';
 
 const AllPosts = () => {
-
   const [postList, setPostList] = useState([]);
-
   useEffect(() => {
     getPostList((response) => {
-      if(response)
-      setPostList(response);
+      if (response) setPostList(response);
     });
   }, []);
 
-
   return (
     <div className='allpost'>
-      {
-        postList.map((post, index) => {
-          var url = post.thumbnail;
-          return (
-         
-            <div key={index}>
-            <HelmetProvider >
-            <Helmet>
-                        <meta  name='category' content={post.category}/>
-                        <meta name='description' content = {post.content}/>
-                    </Helmet>
-                    </HelmetProvider>
- <div className='slider-contain' key={index}>
-<div className='allpost__right-card'>
-<div className='allpost__right-card__img'>
-<img src={cardImg} className='card-image' alt='' />
-</div>
-<div className='allpost__right-card__content'>
-<h5 className='card-title'><strong className='strong-text'>World ◦</strong> &nbsp;Travel ◦</h5>
-  <a href='' className='title'>
-    Want fluffy Japanese pancakes but can’t fly to Tokyo?
-  </a>
-    <ul className='allpost__card-ul'>
-                <li><i className='fa fa-clock-o' aria-hidden='true'> Today</i></li>
-                <li><i className='fa fa-eye' aria-hidden='true'></i> 75 reads</li>
-                <li><i className='fa fa-clock-o' aria-hidden='true'></i> 3 min read</li>
-              </ul>
-</div>
-</div>
-</div>
-
-{/* <div className='allpost-container' key={index}> */}
-{/* <div className='allpost__right-card'>
-<div className='allpost__right-card__img'>
-<img src={url} className='card-image' alt='' />
-</div>
-<div className='allpost__right-card__content'>
-<h5 className='card-title'><strong className='strong-text'>{post.category} ◦</strong></h5>
-  <a href='' className='title'>
-  Want fluffy Japanese pancakes but can’t fly to Tokyo?
-  </a>
-  <div><ReadMore post = {post.content}/></div>
-  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore voluptatem suscipit est id, modi eos odio quis cupiditate sed eligendi.</p>
-    <ul className='allpost__card-ul'>
-                <li><i className='fa fa-clock-o' aria-hidden='true'> { moment(post.datetime).format('ddd-mm-yy')}</i></li>
-                <li><i className='fa fa-eye' aria-hidden='true'></i> 75 reads</li>
-                <li><i className='fa fa-clock-o' aria-hidden='true'></i> 3 min read</li>
-              </ul>
-</div>
-</div>
-
-</div> */}
-</div>
-          )
-  
-        })
-      }
-      <div className='category-component'>
-    <Category/>
-
+      {postList.map((post, index) => {
+        var url = post.thumbnail;
+        return(
+          <div className='allpost__card-list' key={index}>
+            <HelmetProvider>
+              <Helmet>
+                <meta name='category' content={post.category} />
+                <meta name='description' content={post.content} />
+              </Helmet>
+            </HelmetProvider>
+              <div className='allpost__card-list__right-card'>
+                <div className='allpost__card-list__right-card__img'>
+                  <img src={cardImg} className='card-image' alt='' />
+                </div>
+                <div className='allpost__card-list__right-card__content'>
+                  <h5 className='card-title'>
+                    <strong className='strong-text'>World ◦</strong>{' '}
+                    &nbsp;Travel ◦
+                  </h5>
+                  <a href='' className='title'>
+                    Want fluffy Japanese pancakes but can’t fly to Tokyo?
+                  </a>
+                  <ul className='allpost__card-list__card-ul'>
+                    <li>
+                      <i className='fa fa-clock-o' aria-hidden='true'>
+                        Today
+                      </i>
+                    </li>
+                    <li>
+                      <i className='fa fa-eye' aria-hidden='true'></i> 75 reads
+                    </li>
+                    <li>
+                      <i className='fa fa-clock-o' aria-hidden='true'></i> 3 min
+                      read
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        );
+      })}
     </div>
-
-    </div>
-  
-  )
-
-}
+  );
+};
 
 export default React.memo(AllPosts);
