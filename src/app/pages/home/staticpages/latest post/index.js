cbee25264d0f03fcf2cfdc05e54b545ec8a48080
santosh <santoshkumar95432@gmail.com>
2022-04-14 10:05:29 +0530

    scss and data

--- src/app/pages/home/staticpages/latest post/index.js
+++ src/app/pages/home/staticpages/latest post/index.js
@@ -11,68 +11,69 @@ const Latest = () => {
 
   useEffect(() => {
     getPostList((response) => {
-      setPostList(response);
-    })
+      if (response) setPostList(response);
+    });
   }, []);
+
   return (
     <div>
       <div className='allposts'>
         <div className='latest'>
           <button>Latest posts</button>
           <div className='latest__posts'>
-            {
-              postList.map(post => {
-                var url = post.thumbnail;
-                return (
-                  <div className='latest__post' key={post.Blog_id}>
+            {postList.map((post, index) => {
+              return (
+                <div key={index}>
+                  <div className='latest__post'>
                     <div className='latest__post__image'>
-                      <img src={url} key={post.thumbnail} className='posts__image' />
+                      <NavLink to={`/post/${post.title.match(/[a-z]+|\d+/ig).join('-')}/${post.Blog_id}`}>
+                        <img src={post.thumbnail} className='posts__image' alt='' />
+                      </NavLink>
                     </div>
                     <div className='latest__post__maincontent'>
-                      <NavLink className='posts__link' to={`/post/${post.title.match(/[a-z]+|\d+/ig).join('-')}/${post.Blog_id}`}>
+                      <NavLink to={`/post/${post.title.match(/[a-z]+|\d+/ig).join('-')}/${post.Blog_id}`}>
                         {post.category}
                         <h3>{post.title}</h3>
-                      </NavLink>
 
-                      <ul className='post__ul'>
-                        <li><i className='fa fa-clock-o' aria-hidden='true'> {moment(post.date_created).format('MMM-Do-YY, hh:mm A')}</i></li>
-                        <li><i className='fa fa-eye' aria-hidden='true'></i> 75 reads</li>
-                        <li><i className='fa fa-clock-o' aria-hidden='true'></i> 3 min read</li>
-                      </ul>
+                        <ul className='post__ul'>
+                          <li><i className='fa fa-clock-o' aria-hidden='true'> {moment(post.date_created).format('MMM-Do-YY, hh:mm A')}</i></li>
+                          <li><i className='fa fa-eye' aria-hidden='true'></i> 75 reads</li>
+                          <li><i className='fa fa-clock-o' aria-hidden='true'></i> 3 min read</li>
+                        </ul>
+                      </NavLink>
                     </div>
                   </div>
-                )
-              })
+                </div>
+              );
+            })
             }
           </div>
         </div>
         <div className='trending'>
           <button>Categories</button>
           <div className='trending__content'>
-            {
-              postList.map(post => {
-                var url = post.thumbnail;
-                return (
-                  <div className='trending__posts' key={post.Blog_id}>
-                    <div className='trending__posts__image'>
-                      <img src={url} key={post.thumbnail} className='posts__image' />
-                    </div>
-                    <div className='trending__posts__maincontent'>
-                      <NavLink className='posts__link' to={`/post/${post.Blog_id}`}>
-                        {post.category}
-                        <h3>{post.title}</h3>
-                      </NavLink>
-                     
-
-                    </div>
+            {postList.map((post, index) => {
+              return (
+                <div className='trending__posts' key={index}>
+                  <div className='trending__posts__image'>
+                    <NavLink to={`/post/${post.title.match(/[a-z]+|\d+/ig).join('-')}/${post.Blog_id}`}>
+                      <img src={post.thumbnail} className='posts__image' alt='' />
+                    </NavLink>
                   </div>
-                )
-              })
+                  <div className='trending__posts__maincontent'>
+                    <NavLink to={`/post/${post.title.match(/[a-z]+|\d+/ig).join('-')}/${post.Blog_id}`}>
+                      {post.category}
+                      <h3>{post.title}</h3>
+                    </NavLink>
+                  </div>
+                </div>
+              )
+            })
             }
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
